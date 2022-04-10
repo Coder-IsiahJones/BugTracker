5182bff0dd713d54a45ddb13e03f285b3f3a98de
Isiah Jones <jones.isiah.blhs@gmail.com>
2022-04-10 14:14:34 -0400

    Created Charts

--- BugTracker/Controllers/HomeController.cs
+++ BugTracker/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using BugTracker.Extensions;
+﻿using BugTracker.Enums;
+using BugTracker.Extensions;
 using BugTracker.Models;
+using BugTracker.Models.ChartModels;
 using BugTracker.Models.ViewModels;
 using BugTracker.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -78,19 +80,80 @@ namespace BugTracker.Controllers
         {
             int companyId = User.Identity.GetCompanyId().Value;
 
-            List<Project> projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
+            //List<Project> projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
 
             List<object> chartData = new();
             chartData.Add(new object[] { "Priority", "Count" });
 
-
-            foreach (string priority in Enum.GetNames(typeof(ProjectPriority)))
+            foreach (string priority in Enum.GetNames(typeof(ProjectPriorityEnum)))
             {
                 int priorityCount = (await _projectService.GetAllProjectsByPriorityAsync(companyId, priority)).Count();
                 chartData.Add(new object[] { priority, priorityCount });
             }
 
             return Json(chartData);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> AmCharts()
+        {
+            AmChartData amChartData = new();
+            List<AmItem> amItems = new();
+
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            List<Project> projects = (await _companyInfoService.GetAllProjectsAsync(companyId)).Where(p => p.Archived == false).ToList();
+
+            foreach (Project project in projects)
+            {
+                AmItem item = new();
+
+                item.Project = project.Name;
+                item.Tickets = project.Tickets.Count;
+                item.Developers = (await _projectService.GetProjectMembersByRoleAsync(project.Id, nameof(RolesEnum.Developer))).Count();
+
+                amItems.Add(item);
+            }
+
+            amChartData.Data = amItems.ToArray();
+
+            return Json(amChartData.Data);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> PlotlyBarChart()
+        {
+            PlotlyBarData plotlyData = new();
+            List<PlotlyBar> barData = new();
+
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            List<Project> projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
+
+            //Bar One
+            PlotlyBar barOne = new()
+            {
+                X = projects.Select(p => p.Name).ToArray(),
+                Y = projects.SelectMany(p => p.Tickets).GroupBy(t => t.ProjectId).Select(g => g.Count()).ToArray(),
+                Name = "Tickets",
+                Type = "bar"
+            };
+
+            //Bar Two
+            PlotlyBar barTwo = new()
+            {
+                X = projects.Select(p => p.Name).ToArray(),
+                Y = projects.Select(async p => (await _projectService.GetProjectMembersByRoleAsync(p.Id, nameof(RolesEnum.Developer))).Count).Select(c => c.Result).ToArray(),
+                Name = "Developers",
+                Type = "bar"
+            };
+
+            barData.Add(barOne);
+            barData.Add(barTwo);
+
+            plotlyData.Data = barData;
+
+            return Json(plotlyData);
         }
     }
 }
